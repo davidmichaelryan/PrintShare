d3dedb249ef4ad7d079cebec7fe9ad7f7bcc07e9
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-04-23 19:45:07 -0500

    nope

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+import Image
+from tesseract import image_to_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print image_to_string(Image.open('test.png'))
     return 'Hello World!'
