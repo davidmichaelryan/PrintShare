3810245c6a0a3dab9948f7406c378e00406cf7f4
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-04-23 19:40:52 -0500

    nope

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
-# all the imports
 import os
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from flask import Flask
 
-# create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
 
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
+@app.route('/')
+def hello():
+    return 'Hello World!'
