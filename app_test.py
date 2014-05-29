0ab6f0ad99e792af5f2938af7eb7e70df3b4d5d1
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-05-28 19:48:56 -0500

    requirements test

--- app_test.py
+++ app_test.py
@@ -6,10 +6,14 @@ from PIL import Image, ImageFile
 from shutil import rmtree
 from hashlib import sha1
 from flask import request 
-
 import pytesseract
 import google
+
 from flask import Flask, redirect, url_for
+from flask import session, flash, g, \
+     render_template
+from flask_oauth import OAuth
+
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -27,6 +31,9 @@ q3 = ''
 q4 = ''
 
 app = flask.Flask(__name__, static_folder=DATA_DIR)
+app.secret_key = os.urandom(24)
+oauth = OAuth()
+
 
 def safe_addr(ip_addr):
     """Strip of the trailing two octets of the IP address."""
@@ -97,12 +104,144 @@ def crop_ajax():
                 + str(r[2]) + '<br>' 
                 + str(r[0]) + '<br>'
                 + '</div>'
-    + '<a href="http://twitter.com/home/?status=' + str(r[0]) +'">'
-    + '<div class="tweet">'
-    + "Tweet it" + '<br>' 
-                + '</div>'
-                + '</a>' + '<br>')
+        # + '<a href="http://twitter.com/home/?status=' + str(r[0]) +'">'
+          +     '</a><span onclick="tweetLink()" style="color:white;background-color:#318ce7;padding:5px 5px;border-radius:5px;margin-bottom:20px;display:inline-block;">Tweet the link</span>')
+  #     + '<div class="tweet">'
+        # + "Tweet it" + '<br>' 
+  #               + '</div>'
+  #               + '</a>' + '<br>')
+
     return answer
+
+
+
+##===========START TWITTER===========
+
+
+
+# Use Twitter as example remote application
+twitter = oauth.remote_app('twitter',
+    # unless absolute urls are used to make requests, this will be added
+    # before all URLs.  This is also true for request_token_url and others.
+    base_url='https://api.twitter.com/1.1/',
+    # where flask should look for new request tokens
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    # where flask should exchange the token with the remote application
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    # twitter knows two authorizatiom URLs.  /authorize and /authenticate.
+    # they mostly work the same, but for sign on /authenticate is
+    # expected because this will give the user a slightly different
+    # user interface on the twitter side.
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    # the consumer keys from the twitter application registry.
+    consumer_key='PxOmrR580HNL3j5YB1S74t2hp',
+    consumer_secret='vNd8aqeajik3D44PI9dgcTBMPj1cLPFpJ6pyVBkl4uOVau2hBy'
+)
+
+class User():
+    name = ''
+    id = ''
+    oauth_token = ''
+    oauth_secret = ''
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+
+@app.after_request
+def after_request(response):
+    return response
+
+
+@twitter.tokengetter
+def get_twitter_token():
+    """This is used by the API to look for the auth token and secret
+    it should use for API calls.  During the authorization handshake
+    a temporary set of token and secret is used, but afterwards this
+    function has to return the token and secret.  If you don't want
+    to store this in the database, consider putting it into the
+    session instead.
+    """
+    user = g.user
+    if user is not None:
+        return user.oauth_token, user.oauth_secret
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    """Calls the remote twitter API to create a new status update."""
+    if g.user is None:
+        return redirect(url_for('login', next=request.url))
+    status = request.form['tweet']
+    if not status:
+        return redirect(url_for('index'))
+    resp = twitter.post('statuses/update.json', data={
+        'status':       status
+    })
+    if resp.status == 403:
+        flash('Your tweet was too long.')
+    elif resp.status == 401:
+        flash('Authorization error with Twitter.')
+    else:
+        flash('Successfully tweeted your tweet (ID: #%s)' % resp.data['id'])
+    return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+    """Calling into authorize will cause the OpenID auth machinery to kick
+    in.  When all worked out as expected, the remote application will
+    redirect back to the callback URL provided.
+    """
+    return twitter.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('You were signed out')
+    return redirect(request.referrer or url_for('index'))
+
+
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    """Called after authorization.  After this function finished handling,
+    the OAuth information is removed from the session again.  When this
+    happened, the tokengetter from above is used to retrieve the oauth
+    token and secret.
+
+    Because the remote application could have re-authorized the application
+    it is necessary to update the values in the database.
+
+    If the application redirected back after denying, the response passed
+    to the function will be `None`.  Otherwise a dictionary with the values
+    the application submitted.  Note that Twitter itself does not really
+    redirect back unless the user clicks on the application name.
+    """
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    user = User() 
+    user.name = resp['screen_name']
+    user.oauth_token = resp['oauth_token']
+    user.oauth_secret = resp['oauth_token_secret']
+    user.id = resp['user_id']
+
+    session['user'] = user
+
+    flash('You were signed in')
+    return redirect(next_url)
+
+
+
+##===========END TWITTER===========
+
 
     
 @app.route('/')
@@ -293,6 +432,13 @@ dynamically view new images.</noscript>
       file_select_handler(e.target.files[0]);
       e.target.value = '';
   });
+
+function tweetLink(){
+  $.post('/tweet', {tweet: targetURL},
+                            function(reply){
+            $('#status').html('HOLY SHIT YOU TWEETED IT')
+            })
+}
 </script>
 """ 
 
